0399f31e92c7f9417cb45eea861fdf19aca74ca3
Grégoire Paris <postmaster@greg0ire.fr>
2022-02-21 22:28:30 +0100

    Cover AbstractClassMetadataFactory::getParentClasses() (#235)

--- tests/Doctrine/Tests/Persistence/Mapping/AbstractClassMetadataFactoryTest.php
+++ tests/Doctrine/Tests/Persistence/Mapping/AbstractClassMetadataFactoryTest.php
@@ -6,6 +6,8 @@ namespace Doctrine\Tests\Persistence\Mapping;
 
 use Doctrine\Deprecations\PHPUnit\VerifyDeprecations;
 use Doctrine\Persistence\Mapping\AbstractClassMetadataFactory;
+use Doctrine\Persistence\Mapping\ClassMetadata;
+use Doctrine\Persistence\Mapping\Driver\MappingDriver;
 use Doctrine\Tests\DoctrineTestCase;
 
 final class AbstractClassMetadataFactoryTest extends DoctrineTestCase
@@ -31,4 +33,41 @@ final class AbstractClassMetadataFactoryTest extends DoctrineTestCase
         $cmf = $this->getMockForAbstractClass(AbstractClassMetadataFactory::class);
         $cmf->getCacheDriver();
     }
+
+    public function testItSkipsTransientClasses(): void
+    {
+        $cmf = $this->getMockForAbstractClass(AbstractClassMetadataFactory::class);
+        $cmf
+            ->method('newClassMetadataInstance')
+            ->withConsecutive([SomeGrandParentEntity::class], [SomeEntity::class])
+            ->willReturnOnConsecutiveCalls(
+                $this->createMock(ClassMetadata::class),
+                $this->createMock(ClassMetadata::class)
+            );
+        $driver = $this->createMock(MappingDriver::class);
+        $cmf->method('getDriver')
+            ->willReturn($driver);
+
+        $driver->expects($this->exactly(2))
+            ->method('isTransient')
+            ->withConsecutive(
+                [SomeGrandParentEntity::class],
+                [SomeParentEntity::class]
+            )
+            ->willReturnOnConsecutiveCalls(false, true);
+
+        $cmf->getMetadataFor(SomeEntity::class);
+    }
+}
+
+class SomeGrandParentEntity
+{
+}
+
+class SomeParentEntity extends SomeGrandParentEntity
+{
+}
+
+final class SomeEntity extends SomeParentEntity
+{
 }
