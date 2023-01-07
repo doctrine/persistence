a2a42fa573861f18167887512851f6af5b6ec3ce
Vincent Langlet <vincentlanglet@hotmail.fr>
2023-01-07 12:01:19 +0100

    Copy classUtils from doctrine/common

---
+++ src/Persistence/Util/ClassUtils.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Doctrine\Persistence\Util;
+
+use Doctrine\Persistence\Proxy;
+use ReflectionClass;
+
+/**
+ * Class and reflection related functionality for objects that
+ * might or not be proxy objects at the moment.
+ */
+class ClassUtils
+{
+    /**
+     * Gets the real class name of a class name that could be a proxy.
+     *
+     * @param string $className
+     * @psalm-param class-string<Proxy<T>>|class-string<T> $className
+     *
+     * @return string
+     * @psalm-return class-string<T>
+     *
+     * @template T of object
+     */
+    public static function getRealClass($className)
+    {
+        $pos = strrpos($className, '\\' . Proxy::MARKER . '\\');
+
+        if ($pos === false) {
+            /** @psalm-var class-string<T> */
+            return $className;
+        }
+
+        return substr($className, $pos + Proxy::MARKER_LENGTH + 2);
+    }
+
+    /**
+     * Gets the real class name of an object (even if its a proxy).
+     *
+     * @param object $object
+     * @psalm-param Proxy<T>|T $object
+     *
+     * @return string
+     * @psalm-return class-string<T>
+     *
+     * @template T of object
+     */
+    public static function getClass($object)
+    {
+        return self::getRealClass(get_class($object));
+    }
+
+    /**
+     * Gets the real parent class name of a class or object.
+     *
+     * @param string $className
+     * @psalm-param class-string $className
+     *
+     * @return string
+     * @psalm-return class-string
+     */
+    public static function getParentClass($className)
+    {
+        return get_parent_class(self::getRealClass($className));
+    }
+
+    /**
+     * Creates a new reflection class.
+     *
+     * @param string $className
+     * @psalm-param class-string $className
+     *
+     * @return ReflectionClass
+     */
+    public static function newReflectionClass($className)
+    {
+        return new ReflectionClass(self::getRealClass($className));
+    }
+
+    /**
+     * Creates a new reflection object.
+     *
+     * @param object $object
+     *
+     * @return ReflectionClass
+     */
+    public static function newReflectionObject($object)
+    {
+        return self::newReflectionClass(self::getClass($object));
+    }
+
+    /**
+     * Given a class name and a proxy namespace returns the proxy name.
+     *
+     * @param string $className
+     * @param string $proxyNamespace
+     * @psalm-param class-string $className
+     *
+     * @return string
+     * @psalm-return class-string
+     */
+    public static function generateProxyClassName($className, $proxyNamespace)
+    {
+        return rtrim($proxyNamespace, '\\') . '\\' . Proxy::MARKER . '\\' . ltrim($className, '\\');
+    }
+}
---
+++ tests/Persistence/Util/ClassUtilsTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Doctrine\Tests\Persistence\Util
+
+{
+
+    use Doctrine\Persistence\Util\ClassUtils;
+    use Doctrine\Tests\DoctrineTestCase;
+
+    class ClassUtilsTest extends DoctrineTestCase
+    {
+        /**
+         * @psalm-return list<array{class-string, class-string}>
+         */
+        public static function dataGetClass()
+        {
+            return [
+                [\stdClass::class, \stdClass::class],
+                [ClassUtils::class, ClassUtils::class],
+                [ 'MyProject\Proxies\__CG__\stdClass', \stdClass::class],
+                [ 'MyProject\Proxies\__CG__\OtherProject\Proxies\__CG__\stdClass', \stdClass::class],
+                [ 'MyProject\Proxies\__CG__\Doctrine\Tests\Persistence\Util\ChildObject', ChildObject::class],
+            ];
+        }
+
+        /**
+         * @dataProvider dataGetClass
+         */
+        public function testGetRealClass(string $className, string $expectedClassName): void
+        {
+            self::assertEquals($expectedClassName, ClassUtils::getRealClass($className));
+        }
+
+        /**
+         * @dataProvider dataGetClass
+         */
+        public function testGetClass(string $className, string $expectedClassName): void
+        {
+            $object = new $className();
+            self::assertEquals($expectedClassName, ClassUtils::getClass($object));
+        }
+
+        public function testGetParentClass(): void
+        {
+            $parentClass = ClassUtils::getParentClass('MyProject\Proxies\__CG__\OtherProject\Proxies\__CG__\Doctrine\Tests\Persistence\Util\ChildObject');
+            self::assertEquals('stdClass', $parentClass);
+        }
+
+        public function testGenerateProxyClassName(): void
+        {
+            self::assertEquals('Proxies\__CG__\stdClass', ClassUtils::generateProxyClassName('stdClass', 'Proxies'));
+        }
+
+        /**
+         * @dataProvider dataGetClass
+         */
+        public function testNewReflectionClass(string $className, string $expectedClassName): void
+        {
+            $reflClass = ClassUtils::newReflectionClass($className);
+            self::assertEquals($expectedClassName, $reflClass->getName());
+        }
+
+        /**
+         * @dataProvider dataGetClass
+         */
+        public function testNewReflectionObject(string $className, string $expectedClassName): void
+        {
+            $object    = new $className();
+            $reflClass = ClassUtils::newReflectionObject($object);
+            self::assertEquals($expectedClassName, $reflClass->getName());
+        }
+    }
+
+    class ChildObject extends \stdClass
+    {
+    }
+}
+
+namespace MyProject\Proxies\__CG__
+
+{
+    class stdClass extends \stdClass
+    {
+    }
+}
+
+namespace MyProject\Proxies\__CG__\Doctrine\Tests\Persistence\Util
+
+{
+    class ChildObject extends \Doctrine\Tests\Persistence\Util\ChildObject
+    {
+    }
+}
+
+namespace MyProject\Proxies\__CG__\OtherProject\Proxies\__CG__
+
+{
+    class stdClass extends \MyProject\Proxies\__CG__\stdClass
+    {
+    }
+}
+
+namespace MyProject\Proxies\__CG__\OtherProject\Proxies\__CG__\Doctrine\Tests\Persistence\Util
+
+{
+    class ChildObject extends \MyProject\Proxies\__CG__\Doctrine\Tests\Persistence\Util\ChildObject
+    {
+    }
+}
