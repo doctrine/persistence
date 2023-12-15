06b12f1f8c64a53e4014af4d9e8e2e1c95d17b6e
Tom Kaminski <ttkaminski@gmail.com>
2023-12-15 09:51:41 -0600

    Fix whitespace

--- tests/Persistence/Mapping/DriverChainTest.php
+++ tests/Persistence/Mapping/DriverChainTest.php
@@ -63,7 +63,7 @@ class DriverChainTest extends DoctrineTestCase
         $driver1 = $this->createMock(MappingDriver::class);
         $driver1->expects(self::once())
                 ->method('getAllClassNames')
-                ->will(self::returnValue(['Doctrine\Tests\Models\Company\Foo','Doctrine\Tests\Models\Company2\Foo']));
+                ->will(self::returnValue(['Doctrine\Tests\Models\Company\Foo', 'Doctrine\Tests\Models\Company2\Foo']));
 
         $driver2 = $this->createMock(MappingDriver::class);
         $driver2->expects(self::once())
