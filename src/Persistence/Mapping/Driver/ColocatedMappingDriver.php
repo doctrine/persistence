a6b38ce98f1ad6330b2fef0dbde42f9f4c88dc32
Yevhen Sidelnyk <zsidelnik@gmail.com>
2025-08-05 15:46:13 +0300

    Optimize: use O(1) item lookup instead of O(M)
     When checking $includedFiles for an included file,
     the previous `in_array()` approach, executed in a loop,
     is very expensive. Basically, it results in performance
     of O(N * M), where N - number of declared classes,
     M - number of included classes. The current approach
     is O(N), since `isset()` check has constant `O(1)` time.

--- src/Persistence/Mapping/Driver/ColocatedMappingDriver.php
+++ src/Persistence/Mapping/Driver/ColocatedMappingDriver.php
@@ -19,7 +19,6 @@ use function array_merge;
 use function array_unique;
 use function assert;
 use function get_declared_classes;
-use function in_array;
 use function is_dir;
 use function preg_match;
 use function preg_quote;
@@ -158,7 +157,8 @@ trait ColocatedMappingDriver
         }
 
         $sourceFilePathNames = $this->pathNameIterator($filesIterator);
-        $includedFiles       = [];
+        /** @var array<string,true> $includedFiles */
+        $includedFiles = [];
 
         foreach ($sourceFilePathNames as $sourceFile) {
             if (preg_match('(^phar:)i', $sourceFile) === 0) {
@@ -179,7 +179,7 @@ trait ColocatedMappingDriver
 
             require_once $sourceFile;
 
-            $includedFiles[] = $sourceFile;
+            $includedFiles[$sourceFile] = true;
         }
 
         $classes  = [];
@@ -190,7 +190,7 @@ trait ColocatedMappingDriver
 
             $sourceFile = $rc->getFileName();
 
-            if (! in_array($sourceFile, $includedFiles, true) || $this->isTransient($className)) {
+            if (! isset($includedFiles[$sourceFile]) || $this->isTransient($className)) {
                 continue;
             }
 
